321b57e326ece906875436e2d7c36586c9481429
0111001101110011 <gapm55@gmail.com>
2020-02-09 22:12:59 -0700

    1

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour, IFlip
+{
+    [SerializeField]
+    float moveSpeed = 3f;
+    [SerializeField]
+    float delay;
+    float timer;
+
+    [SerializeField]
+    Vector2 dir;
+
+    void Update()
+    {
+        transform.Translate(dir * moveSpeed * Time.deltaTime);
+
+        timer +=  Time.deltaTime;
+        if(timer >= delay)
+        {
+            timer = 0f;
+            //dir = dir == Vector2.right ? Vector2.left : Vector2.right;
+            dir.x = dir.x > 0 ? -1 : 1;
+        }
+    }
+
+    public void FlipSprite()
+    {
+        
+    }
+}
